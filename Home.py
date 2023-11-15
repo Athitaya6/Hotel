59bdefbf3f1188eb903d267683c74eab17a569dd
Athitaya123 <athitayakakandee@gmail.com>
2023-11-15 09:03:47 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,9 +8,9 @@ st.markdown("----")
 col1, col2 = st.columns(2)
 #col1.write("This is column 1")
 #col2.write("This is column 2")
-with col1:
+#with col1:
     #st.image('./pic/k1.jpg')
-with col2:
+#with col2:
     #st.image('./pic/iris.jpg')
     
 
