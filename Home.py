0578214e2e72751543e3501bcddd3d8668421d7d
Athitaya123 <athitayakakandee@gmail.com>
2023-11-15 09:20:23 +0700

    .commit

--- Home.py
+++ Home.py
@@ -16,7 +16,7 @@ col1, col2 = st.columns(2)
 
 html_1="""
 <div style="background-color:#63D767;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+<center><h5>การจองโรงแรม</h5></center>
 </div>
 """    
 st.markdown(html_1, unsafe_allow_html=True)
@@ -24,16 +24,29 @@ st.markdown("")
 
 import pandas as pd
 
-dt=pd.read_xlsx('data/CleanHotel.xlsx')
+dt=pd.read_csv('data/CleanHotel.csv')
 st.write(dt.head(10))
 
-dt1 = dt['petal.length'].sum()
-dt2 = dt['petal.width'].sum()
-dt3 = dt['sepal.length'].sum()
-dt4 = dt['sepal.width'].sum()
+dt1 = dt['no adults'].sum()
+dt2 = dt['no children'].sum()
+dt3 = dt['weekend ss'].sum()
+dt4 = dt['week mf'].sum()
+dt5 = dt['meal plan'].sum()
+dt6 = dt['car parking'].sum()
+dt7 = dt['leadtime'].sum()
+dt8 = dt['arrival year'].sum()
+dt9 = dt['arrival month'].sum()
+dt10 = dt['arrival date'].sum()
+dt11 = dt['market segment'].sum()
+dt12 = dt['repeated guest'].sum()
+dt13 = dt['cancellations'].sum()
+dt14 = dt['bookings not canceled'].sum()
+dt15 = dt['avg price'].sum()
+dt16 = dt['special requests'].sum()
+dt17 = dt['booking_status'].sum()
 
-dx = [dt1, dt2, dt3, dt4]
-dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
+dx = [dt1, dt2, dt3, dt4, dt5, dt6, dt7, dt8, dt9, dt10, dt11, dt12, dt13, dt14, dt15, dt16, dt17]
+dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4", "dt5", "dt6", "dt7", "dt8", "dt9", "dt10", "dt11", "dt12", "dt13", "dt14", "dt15", "dt16", "dt17"])
 
 if st.button("show bar chart"):
    st.bar_chart(dx2)
@@ -44,14 +57,14 @@ else :
    
 html_2 = """
 <div style="background-color:#6CE9E8;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>การทำนายคลาสดอกไม้</h5></center>
+<center><h5>การทำนายพฤติกรรมการจองโรงแรม</h5></center>
 </div>
 """
 st.markdown(html_2, unsafe_allow_html=True)
 st.markdown("")
 
-ptlen = st.slider("กรุณาเลือกข้อมูล petal.length",0,10)
-ptwd = st.slider("กรุณาเลือกข้อมูล petal.width",0,10)
+ptlen = st.slider("จำนวนผู้ใหญ่ no adults",0,10)
+ptwd = st.slider("จำนวนเด็ก no children",0,10)
 
 splen = st.number_input("กรุณาเลือกข้อมูล sepal.length")
 spwd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
