fea1e08c5e3df7c60e7af0b26a62093fde69a1b5
Athitaya123 <athitayakakandee@gmail.com>
2023-11-15 09:01:02 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -9,9 +9,9 @@ col1, col2 = st.columns(2)
 #col1.write("This is column 1")
 #col2.write("This is column 2")
 with col1:
-    st.image('./pic/k1.jpg')
+    #st.image('./pic/k1.jpg')
 with col2:
-    st.image('./pic/iris.jpg')
+    #st.image('./pic/iris.jpg')
     
 
 html_1="""
