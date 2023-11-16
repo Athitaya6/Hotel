81f4812d183d6bbc3c7eb5383977b43fdfcb9a7b
Athitaya123 <athitayakakandee@gmail.com>
2023-11-16 10:57:05 +0700

    .commit

---
+++ pages/2_MyProject.py
@@ -0,0 +1,15 @@
+import json
+import time
+import requests
+import streamlit as st
+
+st.set_page_config(
+    page_title="Athitaya Datascience Project",
+    page_icon= ":bar_chart:",
+)
+st.sidebar.success("เลือกรายการด้านบน.")
+
+st.write("# พฤติกรรมการจองโรงแรมของลูกค้า! 💸  📉 ")
+st.write("### 1.หลักการและเหตุผล")
+st.write("### 2.วัตถุประสงค์")
+st.balloons()
