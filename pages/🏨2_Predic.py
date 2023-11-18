eee13c3aadbc8299e160d1bea8f4f85e9bc59ce5
Athitaya123 <athitayakakandee@gmail.com>
2023-11-18 19:07:33 +0700

    .commit

---
+++ pages/🏨2_Predic.py
@@ -0,0 +1,108 @@
+import streamlit as st 
+
+st.title('พฤติกรรมการจองโรงแรมของลูกค้า')
+st.header('นางสาวอฑิตยา กะการดี')
+st.subheader('สาขาวิทยาการข้อมูล')
+st.markdown("----")
+
+col1, col2 = st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
+#with col1:
+    #st.image('./pic/k1.jpg')
+#with col2:
+    #st.image('./pic/iris.jpg')
+    
+
+html_1="""
+<div style="background-color:#63D767;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>การจองโรงแรม</h5></center>
+</div>
+"""    
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
+
+import pandas as pd
+
+dt=pd.read_csv('.\data\MinMax.csv')
+st.write(dt.head(10))
+
+dt1 = dt['no adults'].sum()
+dt2 = dt['no children'].sum()
+dt3 = dt['weekend ss'].sum()
+dt4 = dt['week mf'].sum()
+dt5 = dt['meal plan'].sum()
+dt6 = dt['car parking'].sum()
+dt7 = dt['leadtime'].sum()
+dt8 = dt['arrival year'].sum()
+dt9 = dt['arrival month'].sum()
+dt10 = dt['arrival date'].sum()
+dt11 = dt['market segment'].sum()
+dt12 = dt['repeated guest'].sum()
+dt13 = dt['cancellations'].sum()
+dt14 = dt['bookings not canceled'].sum()
+dt15 = dt['avg price'].sum()
+dt16 = dt['special requests'].sum()
+dt17 = dt['booking_status'].sum()
+
+dx = [dt1, dt2, dt3, dt4, dt5, dt6, dt7, dt8, dt9, dt10, dt11, dt12, dt13, dt14, dt15, dt16, dt17]
+dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4", "dt5", "dt6", "dt7", "dt8", "dt9", "dt10", "dt11", "dt12", "dt13", "dt14", "dt15", "dt16", "dt17"])
+
+if st.button("show bar chart"):
+   st.bar_chart(dx2)
+   st.button("Not show bar chart")
+else :
+   st.button("Not show bar chart")
+   
+   
+html_2 = """
+<div style="background-color:#6CE9E8;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>การทำนายพฤติกรรมการจองโรงแรม</h5></center>
+</div>
+"""
+st.markdown(html_2, unsafe_allow_html=True)
+st.markdown("")
+
+hadults = st.slider("จำนวนผู้ใหญ่ no adults",0,10)
+hchildren = st.slider("จำนวนเด็ก no children",0,10)
+hweekend = st.slider("วันเสาร์-อาทิตย์ weekend ss",0,2)
+hweek = st.slider("วันจันทร์-ศุกร์ week mf",0,5)
+hcar = st.slider("ที่จอดรถ car parking",0,1)
+
+htime = st.number_input("จำนวนวันตั้งแต่จอง leadtime")
+hmonth = st.number_input("เดือนที่จอง arrival month")
+hdate = st.number_input("วันที่จอง arrival date")
+hspecial = st.number_input("คำขอพิเศษ special requests")
+
+from sklearn.tree import DecisionTreeClassifier
+import numpy as np
+
+if st.button("ทำนายผล"):
+    #ทำนาย
+    dt = pd.read_csv(".\data\MinMax.csv") 
+    X = dt.drop('variety', axis=1)
+    y = dt.variety 
+    st.button("ไม่ทำนายผล")
+
+    DTT_model = DecisionTreeClassifier (criterion='gini')
+    DTT_model.fit(X, y)
+
+#ข้อมูลสำหรับทดลองจำแนกข้อมูล
+    x_input = np.array([hadults, hchildren, hweekend, hweek, hcar, htime, hmonth, hdate, hspecial])
+
+#เอา input ไปทดสอบ
+    st.write(DTT_model.predict(x_input))
+    out=DTT_model.predict(x_input)
+
+    if out[0]=="Not_Canceled":    
+        st.image("./image/1.jpg")
+        st.header("Not_Canceled")
+    elif out[0]=="Canceled":
+        st.image("./image/2.jpg")
+        st.header("Canceled")
+    else:
+        st.image("./image/2.jpg") 
+        st.header("Canceled") 
+    st.button("ไม่ทำนาย")
+else:
+    st.button("ไม่ทำนาย")
