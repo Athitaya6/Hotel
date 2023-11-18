2f761cc86f2bc99383a4022a5751e616491c6382
Athitaya123 <athitayakakandee@gmail.com>
2023-11-18 19:35:51 +0700

    Create Project.py

---
+++ Project.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+with st.sidebar:
+    selected = option_menu(
+        menu_title= "Main Menu",
+        option=["Home","Prediction"]
+    )
