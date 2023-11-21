ab7ab035435b2807efba002e1f91d1e7e8b7f6f4
Athitaya123 <athitayakakandee@gmail.com>
2023-11-21 19:29:09 +0700

    Delete 123.py

--- pages/123.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st 
-from streamlit_option_menu import option_menu
-
-
-with st.sidebar:
-    selected = option_menu(
-        menu_title="Main Menu",
-        option=["Home", "Thoery", "Prediction"],
-    )
