c894657d5274e5f7ab12e775a7844a5b76229fa0
Athitaya123 <athitayakakandee@gmail.com>
2023-11-21 09:56:50 +0700

    .

--- pages/🏨2_Predic.py
+++ pages/🏨2_Predic.py
@@ -87,24 +87,24 @@ if st.button("ทำนายผล"):
 
     DTT_model = DecisionTreeClassifier (criterion='gini')
     DTT_model.fit(X, y)
-
+    st.write(X.head(4))
 #ข้อมูลสำหรับทดลองจำแนกข้อมูล
     x_input = np.array([[hadults, hchildren, hweekend, hweek, hcar, htime, hmonth, hdate, hspecial]])
     #st.write(x_input)
 #เอา input ไปทดสอบ
-    st.write(DTT_model.predict(x_input))
-    out=DTT_model.predict(x_input)
+    #st.write(DTT_model.predict(x_input))
+    #out=DTT_model.predict(x_input)
 
-    if out[0]=="Not_Canceled":    
-        st.image("./image/1.jpg")
-        st.header("Not_Canceled")
-    elif out[0]=="Canceled":
-        st.image("./image/2.jpg")
-        st.header("Canceled")
-    else:
-        st.image("./image/2.jpg") 
-        st.header("Canceled") 
-    st.button("ไม่ทำนาย")
+    #if out[0]=="Not_Canceled":    
+    #    st.image("./image/1.jpg")
+    #    st.header("Not_Canceled")
+    #elif out[0]=="Canceled":
+    #    st.image("./image/2.jpg")
+    #    st.header("Canceled")
+    #else:
+    #    st.image("./image/2.jpg") 
+    #    st.header("Canceled") 
+    #st.button("ไม่ทำนาย")
 else:
     st.button("ไม่ทำนาย")
     
