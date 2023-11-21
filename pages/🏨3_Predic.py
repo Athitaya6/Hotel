e802e41aa145b2c22ac7a5efdbe7a3bc19959624
Athitaya123 <athitayakakandee@gmail.com>
2023-11-21 19:50:54 +0700

    Update 🏨3_Predic.py

--- pages/🏨3_Predic.py
+++ pages/🏨3_Predic.py
@@ -6,8 +6,8 @@ st.subheader('สาขาวิทยาการข้อมูล')
 st.markdown("----")
 
 col1, col2 = st.columns(2)
-col1.write("This is column 1")
-col2.write("This is column 2")
+#col1.write("This is column 1")
+#col2.write("This is column 2")
 with col1:
     st.image('./image/Aa.jpg')
 with col2:
